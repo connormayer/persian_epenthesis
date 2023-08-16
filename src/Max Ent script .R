39fd3cef148d15226695e4f6ac93452f266cd58e
nkhaloo <noahkhaloo@gmail.com>
2023-08-16 14:48:03 -0700

    tried to load each file from directory

--- src/Max Ent script .R
+++ src/Max Ent script .R
@@ -48,3 +48,24 @@ results$predictions
 #compare models 
 compare_models(fh_model, gs_model, gc_model, method = "bic")
 
+
+#run max ent on each individual participant
+#fleischhacker
+library(fs)
+
+fh_file_path <- fs::dir_ls("tableaux/fleischhacker")
+fh_file_path
+
+fh_file_contents <- list()
+for(i in seq_along(fh_file_path)) {
+  fh_file_contents[[i]] <- read_csv(
+    file = fh_file_path[[i]]
+  )
+}
+
+fh_file_contents <- set_names(fh_file_contents, c("fh_global.csv", "fh_p1.csv", "fh_p2.csv", "fh_p3.csv", "fh_p4.csv", "fh_p5.csv", "fh_p6.csv", "fh_p7.csv", "fh_p8.csv", "fh_p9.csv", "fh_p10.csv", "fh_p13.csv", "fh_p14.csv", "fh_p15.csv", "fh_p16.csv", "fh_p17.csv", "fh_p18.csv", "fh_p19.csv", "fh_p20.csv", "fh_p21.csv","fh_template.csv"))
+
+
+lapply(fh_file_contents, optimize_weights)
+
+
